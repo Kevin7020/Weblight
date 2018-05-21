139d46fcdb4f2867cf6751437710f54e0aa19f53
Kevin Porras R <kevin702010@hotmail.com>
2018-05-20 19:54:46 -0600

    Initial Commit

---
+++ ESP_Weblight.ino
@@ -0,0 +1,235 @@
+/*
+  ESP_WebConfig
+
+  Copyright (c) 2015 John Lassen. All rights reserved.
+  This is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This software is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Latest version: 1.1.3  - 2015-07-20
+  Changed the loading of the Javascript and CCS Files, so that they will successively loaded and that only one request goes to the ESP.
+
+  -----------------------------------------------------------------------------------------------
+  History
+
+  Version: 1.1.2  - 2015-07-17
+  Added URLDECODE for some input-fields (SSID, PASSWORD...)
+
+  Version  1.1.1 - 2015-07-12
+  First initial version to the public
+
+
+
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <Ticker.h>
+#include <EEPROM.h>
+#include <WiFiUdp.h>
+#include "helpers.h"
+#include "global.h"
+/*
+  Include the HTML, STYLE and Script "Pages"
+*/
+#include "Page_Root.h"
+#include "Page_Admin.h"
+#include "Page_Script.js.h"
+#include "Page_Style.css.h"
+#include "Page_NTPsettings.h"
+#include "Page_Information.h"
+#include "Page_General.h"
+#include "PAGE_NetworkConfiguration.h"
+#include "example.h"
+#include "credentials.h"
+
+#define ACCESS_POINT_NAME  "ESP_8266"
+#define ACCESS_POINT_PASSWORD  "12345678"
+#define AdminTimeOut 180  // Defines the Time in Seconds, when the Admin-Mode will be diabled
+#define LED_BUILTIN 2
+#define PWM_LED 5
+
+
+void setup ( void ) {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PWM_LED, OUTPUT);
+  EEPROM.begin(512);
+  Serial.begin(115200);
+  delay(500);
+  Serial.println("Starting ES8266");
+  if (!ReadConfig())
+  {
+    // DEFAULT CONFIG
+    config.ssid = mySSID;
+    config.password = myPASSWORD;
+    config.dhcp = true;
+    config.IP[0] = 192; config.IP[1] = 168; config.IP[2] = 1; config.IP[3] = 100;
+    config.Netmask[0] = 255; config.Netmask[1] = 255; config.Netmask[2] = 255; config.Netmask[3] = 0;
+    config.Gateway[0] = 192; config.Gateway[1] = 168; config.Gateway[2] = 1; config.Gateway[3] = 1;
+    config.ntpServerName = "1.cr.pool.ntp.org";
+    config.Update_Time_Via_NTP_Every = 1440;
+    config.timezone = -6;
+    config.daylight = false;
+    config.DeviceName = "Not Named";
+    config.AutoTurnOff = false;
+    config.AutoTurnOn = false;
+    config.TurnOffHour = 0;
+    config.TurnOffMinute = 0;
+    config.TurnOnHour = 0;
+    config.TurnOnMinute = 0;
+    WriteConfig();
+    Serial.println("Default config applied");
+  }
+
+
+  if (AdminEnabled)
+  {
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.softAP( ACCESS_POINT_NAME , ACCESS_POINT_PASSWORD);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  else
+  {
+    WiFi.mode(WIFI_STA);
+  }
+
+  ConfigureWifi();
+
+
+  server.on ( "/", processExample  );
+  server.on ( "/admin/filldynamicdata", filldynamicdata );
+
+  server.on ( "/favicon.ico",   []() {
+    Serial.println("favicon.ico");
+    server.send ( 200, "text/html", "" );
+  }  );
+
+
+  server.on ( "/admin.html", []() {
+    Serial.println("admin.html");
+    server.send ( 200, "text/html", PAGE_AdminMainPage );
+  }  );
+  server.on ( "/config.html", send_network_configuration_html );
+  server.on ( "/info.html", []() {
+    Serial.println("info.html");
+    server.send ( 200, "text/html", PAGE_Information );
+  }  );
+  server.on ( "/ntp.html", send_NTP_configuration_html  );
+  server.on ( "/general.html", send_general_html  );
+  //	server.on ( "/example.html", []() { server.send ( 200, "text/html", PAGE_EXAMPLE );  } );
+  server.on ( "/style.css", []() {
+    Serial.println("style.css");
+    server.send ( 200, "text/plain", PAGE_Style_css );
+  } );
+  server.on ( "/microajax.js", []() {
+    Serial.println("microajax.js");
+    server.send ( 200, "text/plain", PAGE_microajax_js );
+  } );
+  server.on ( "/admin/values", send_network_configuration_values_html );
+  server.on ( "/admin/connectionstate", send_connection_state_values_html );
+  server.on ( "/admin/infovalues", send_information_values_html );
+  server.on ( "/admin/ntpvalues", send_NTP_configuration_values_html );
+  server.on ( "/admin/generalvalues", send_general_configuration_values_html);
+  server.on ( "/admin/devicename",     send_devicename_value_html);
+
+
+
+
+  server.onNotFound ( []() {
+    Serial.println("Page Not Found");
+    server.send ( 400, "text/html", "Page not Found" );
+  }  );
+  server.begin();
+  Serial.println( "HTTP server started" );
+  tkSecond.attach(1, Second_Tick);
+  UDPNTPClient.begin(2390);  // Port for NTP receive
+}
+
+  void Led_fade( void ){
+    int brightness = 0;    // how bright the LED is
+    int fadeAmount = 5;    // how many points to fade the LED by
+
+    for (int i=0; i <= 3; i++){ // the loop routine
+      // set the brightness of pin PWM_LED:
+      analogWrite(PWM_LED, brightness);
+
+      // change the brightness for next time through the loop:
+      brightness = brightness + fadeAmount;
+
+      // reverse the direction of the fading at the ends of the fade:
+      if (brightness <= 0 || brightness >= 255) {
+        fadeAmount = -fadeAmount;
+      }
+      delay(30); // wait for 30 milliseconds to see the dimming effect
+    }
+  }
+
+void loop ( void ) {
+  if (AdminEnabled)
+  {
+    if (AdminTimeOutCounter > AdminTimeOut)
+    {
+      AdminEnabled = false;
+      Serial.println("Admin Mode disabled!");
+      WiFi.mode(WIFI_STA);
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+  }
+  if (config.Update_Time_Via_NTP_Every  > 0 )
+  {
+    if (cNTP_Update > 5 && firstStart)
+    {
+      NTPRefresh();
+      cNTP_Update = 0;
+      firstStart = false;
+    }
+    else if ( cNTP_Update > (config.Update_Time_Via_NTP_Every * 60) )
+    {
+
+      NTPRefresh();
+      cNTP_Update = 0;
+    }
+  }
+
+  if (DateTime.minute != Minute_Old)
+  {
+    Minute_Old = DateTime.minute;
+    if (config.AutoTurnOn)
+    {
+      if (DateTime.hour == config.TurnOnHour && DateTime.minute == config.TurnOnMinute)
+      {
+        Serial.println("SwitchON");
+      }
+    }
+
+
+    Minute_Old = DateTime.minute;
+    if (config.AutoTurnOff)
+    {
+      if (DateTime.hour == config.TurnOffHour && DateTime.minute == config.TurnOffMinute)
+      {
+        Serial.println("SwitchOff");
+      }
+    }
+  }
+  server.handleClient();
+
+//cd
+
+  if (Refresh) {
+    Refresh = false;
+    ///Serial.println("Refreshing...");
+    //Serial.printf("FreeMem:%d %d:%d:%d %d.%d.%d \n",ESP.getFreeHeap() , DateTime.hour,DateTime.minute, DateTime.second, DateTime.year, DateTime.month, DateTime.day);
+  }
+
+}
